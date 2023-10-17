c72f53427e15d025ad00f38e00912402c6363122
b0 <bewuket1621@gmail.com>
2023-10-17 10:20:24 +0300

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,24 @@ import React from "react";
 const About = () => {
   return (
     <div className=" h-screen border border-yellow-800" id="about">
-      About
+      <div className="border my-20 flex mx-10">
+        <div className="border -my-10">
+          <img
+            src="/personal/pexels-italo-melo-2379004-removebg-preview.png"
+            alt=""
+          />
+        </div>
+        <div className=" space-y-10 w-[650px] border mx-10 my-20">
+          <h2 className=" text-4xl text-center">About Me</h2>
+          <p>
+            Howdy, My Name Is Wadee-_-Haddad A Cyber-Security Student At Amman
+            Arab University, Born On May 25, 2004. I've Accomplished Several
+            Notable Achievements, Including Receiving An Academic Acceleration
+            From The Ministry Of Education In Jordan.
+          </p>
+          <button>Download Cv</button>
+        </div>
+      </div>
     </div>
   );
 };
