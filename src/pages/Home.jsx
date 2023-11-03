f10ef7d036fd72e9cfe3c50058a66e739624a98d
b0 <bewuket1621@gmail.com>
2023-11-03 16:49:11 +0300

    made the content on home page in the center

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,37 +28,40 @@ const Home = () => {
           alt=""
         />
       </div>
-      <div className="flex flex-col  md:flex-row justify-around  w-[88%] mx-auto  align-middle mt-[12%] md:mt-[8%]">
-        <div
-          className="  w-full flex flex-col justify-center align-middle"
-          data-aos="fade-down-right"
-        >
-          <div className="text-4xl md:text-6xl">
-            Hello 👋, <br /> I'm
-            <span className=" text-7xl px-4 text-green-700">David B</span>,{" "}
-            <br /> Software Developer
+      <div className=" min-h-full flex flex-col justify-center">
+        <div className="flex flex-col  md:flex-row justify-around  w-[88%] mx-auto  align-middle pb-20 ">
+          <div
+            className="  w-full flex flex-col justify-center align-middle"
+            data-aos="fade-down-right"
+          >
+            <div className="text-4xl md:text-6xl">
+              Hello 👋, <br /> I'm
+              <span className=" text-7xl px-4 text-green-700">
+                David B
+              </span>, <br /> Software Developer
+            </div>
+            <div className=" text-xl my-9 text-slate-400">
+              I specialize in full-stack web development and have a keen
+              interest in cybersecurity.{" "}
+            </div>
+            <div className="text-base text-green-700 hover:text-green-800 cursor-pointer">
+              <Link to="about" smooth={true} duration={1500}>
+                More About me{" "}
+                <span className=" text-lg mt-1 hover⚖ ">&darr;</span>
+              </Link>
+            </div>
           </div>
-          <div className=" text-xl my-9 text-slate-400">
-            I specialize in full-stack web development and have a keen interest
-            in cybersecurity.{" "}
+          <div
+            className="flex justify-start  w-full"
+            // data-aos="fade-up-left"
+            data-aos="zoom-in"
+          >
+            <img
+              className=" md:-ml-20 ml-0 mt-0 md:-mt-20 top p-3 h-[130%]"
+              src="./christian-buehner-DItYlc26zVI-unsplash-removebg-preview.png"
+              alt=""
+            />
           </div>
-          <div className="text-base text-green-700 hover:text-green-800 cursor-pointer">
-            <Link to="about" smooth={true} duration={1500}>
-              More About me{" "}
-              <span className=" text-lg mt-1 hover⚖ ">&darr;</span>
-            </Link>
-          </div>
-        </div>
-        <div
-          className="flex justify-start  w-full"
-          // data-aos="fade-up-left"
-          data-aos="zoom-in"
-        >
-          <img
-            className=" -ml-28 mt-0 md:-mt-32 top p-3 h-[130%]"
-            src="./christian-buehner-DItYlc26zVI-unsplash-removebg-preview.png"
-            alt=""
-          />
         </div>
       </div>
     </div>
