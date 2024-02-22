b5935e9e21f8d40ffc8c5da41568032a92cea410
b0 <bewuket1621@gmail.com>
2024-02-22 14:38:38 +0300

    add a showmore button

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-scroll";
 const About = () => {
+  const [isTextExpanded, setIsTextExpanded] = useState(false);
+
+  const toggleTextVisibility = () => {
+    setIsTextExpanded(!isTextExpanded);
+  };
   return (
-    <div className=" min-h-screen section " id="about" name="about">
-      <div className=" flex flex-col h-full justify-center">
-        <div className="  flex md:mx-14 mb-10 ">
+    <div className="section min-h-screen" id="about" name="about">
+      <div className="flex h-full flex-col justify-center">
+        <div className="mb-10 flex md:mx-14">
           <div
-            className="  scale-110 hidden md:block "
+            className="hidden scale-110 md:block"
             data-aos="zoom-in-right"
           >
             <img src="/pers/msg432754405-44032-removebg-preview.png" alt="" />
@@ -17,11 +23,11 @@ const About = () => {
           </div>
           <div
             data-aos="fade-left"
-            className=" space-y-7 w-[650px]  mx-10 my-10 text-base text-lg"
+            className="mx-10 my-10 w-[650px] space-y-7 text-base text-lg"
           >
             <div className="mt-10">
-              <h2 className=" text-3xl md:text-5xl text-center">About Me</h2>
-              <div className="border-b border-green-700 w-14 mt-1 md:w-24 mx-auto"></div>
+              <h2 className="text-center text-3xl md:text-5xl">About Me</h2>
+              <div className="mx-auto mt-1 w-14 border-b border-green-700 md:w-24"></div>
             </div>
             {/* <p className=" ">
 
@@ -61,33 +67,46 @@ const About = () => {
               teams to bring ideas to life because I firmly believe in the power
               of teamwork.
             </p>
-            <p>
-              When I'm not immersed in lines of code, you'll find me exploring
-              new technologies, attending tech meetups, or seeking inspiration
-              from diverse sources to infuse creativity into my projects.
-            </p>
-            <p>
-              I'm thrilled to embark on new challenges and contribute my
-              expertise to innovative projects.{" "}
-              <Link
-                to="contact"
-                smooth={true}
-                duration={2000}
-                className=" text-green-700 hover:cursor-pointer"
+
+            {isTextExpanded ? (
+              <>
+                <p>
+                  When I'm not immersed in lines of code, you'll find me
+                  exploring new technologies, attending tech meetups, or seeking
+                  inspiration from diverse sources to infuse creativity into my
+                  projects.
+                </p>
+                <p>
+                  I'm thrilled to embark on new challenges and contribute my
+                  expertise to innovative projects.{" "}
+                  <Link
+                    to="contact"
+                    smooth={true}
+                    duration={2000}
+                    className="text-green-700 hover:cursor-pointer"
+                  >
+                    Let's connect{" "}
+                  </Link>
+                  and build something remarkable together!"
+                </p>
+              </>
+            ) : (
+              <button
+                onClick={toggleTextVisibility}
+                className="show-more-button text-green-700 hover:cursor-pointer"
               >
-                Let's connect{" "}
-              </Link>
-              and build something remarkable together!"
-            </p>
+                Show More
+              </button>
+            )}
             {/* <div className=" ">
-              <a
-                className=" px-5 py-2 border rounded-md border-green-700 text-white duration-300 bg-green-800 hover:bg-green-900 "
+            <a
+                className="rounded-md border border-green-700 bg-green-800 px-5 py-2 text-white duration-300 hover:bg-green-900"
                 href="/pdf/Booktree.ngLinux-Basics-for-Hackers.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <img
-                  className="inline w-4 mr-2 mb-1"
+                  className="mb-1 mr-2 inline w-4"
                   src="/settings/download-2-24.ico"
                   alt=""
                 />
